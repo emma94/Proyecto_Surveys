150ee5b9d33b2c2acd8ab0514d52043221fc6370
emma94 <emmanuel.jimenez@ucrso.info>
2016-06-08 16:32:38 -0600

    Otros cambios js

--- resources/views/masterPage.blade.php
+++ resources/views/masterPage.blade.php
@@ -10,7 +10,7 @@
 
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js" type="text/javascript"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src=/js/bootstrap.min.js" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <script src="/js/jquery.sortable.js"></script>
     <script src="/js/jquery.bootpag.min.js"></script>
