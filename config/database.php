ee2ea9432ea39fe86d824609858d509996ab91c6
yohandl17 <yohan.diaz@ucrso.info>
2016-06-07 21:12:25 -0600

    database modificada again

--- config/database.php
+++ config/database.php
@@ -1,10 +1,13 @@
 <?php
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = $url["host"];
+
+$server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$database = substr($url["path"], 1);
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 
 return [
 
