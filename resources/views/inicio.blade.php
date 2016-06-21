0513b455a130ea641de67c9010685ae641360a6c
emma94 <emmanuel.jimenez@ucrso.info>
2016-06-21 13:31:52 -0600

    Imagen responsive

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     @if (!Auth::guest())
         <div>
-            <img src="/imgs/info2.png" alt="Info" style="width: 1140px;">
+            <img src="/imgs/info2.png" alt="Info" style="width: 100%;">
         </div>
     @else
         <div>
-            <img src="/imgs/info.png" alt="Info" style="width: 1140px;">
+            <img src="/imgs/info.png" alt="Info" style="width: 100%;">
         </div>
     @endif
 @stop
