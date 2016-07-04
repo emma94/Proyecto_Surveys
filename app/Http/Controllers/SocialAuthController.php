56a34b7d45f5a62dd4853862eb096e05f714ac25
yohandl17 <yohan.diaz@ucrso.info>
2016-07-04 09:25:53 -0600

    arreglo login facebook
    se caia cuando le daba cancelar en facebook y el callback venia null

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\SocialAccountService;
+use Mockery\CountValidator\Exception;
 use Socialite;
 
 class SocialAuthController extends Controller
@@ -23,10 +24,12 @@ class SocialAuthController extends Controller
 
     public function callback(SocialAccountService $service)
     {
-        $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
-
-        auth()->login($user);
-
-        return redirect()->to('/');
+        try {
+            $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
+            auth()->login($user);
+            return redirect()->to('/');
+        } catch (Exception $ex) {
+            return view("inicio");
+        }
     }
 }
